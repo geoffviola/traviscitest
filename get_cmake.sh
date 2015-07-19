e207dc4373cc94a948e61b339cb3c04601de3069
Geoffrey Viola <geoffrey.viola@asirobots.com>
2015-07-18 18:59:59 -0600

    download the cmake binary for speed and only support linux for simplicity

--- get_cmake.sh
+++ get_cmake.sh
@@ -1,9 +1,4 @@
 #!/bin/sh
-curl -O "http://www.cmake.org/files/v3.3/cmake-3.3.0-rc4.tar.gz"
-tar xfz "cmake-3.3.0-rc4.tar.gz"
-cd "cmake-3.3.0-rc4"
-mkdir build
-cd build
-cmake ..
-make -j5
-cd ../..
+curl -O "http://www.cmake.org/files/v3.3/cmake-3.3.0-rc4-Linux-x86_64.tar.gz"
+tar xfz "cmake-3.3.0-rc4-Linux-x86_64.tar.gz"
+cd "cmake-3.3.0-rc4-Linux-x86_64"
