ce847ed296b0d0d8e10f4d834361585636d87b7d
Geoffrey Viola <geoffrey.viola@asirobots.com>
2015-07-18 18:31:11 -0600

    changed options for gzip and added debug info

--- get_cmake.sh
+++ get_cmake.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 curl -O "http://www.cmake.org/files/v3.3/cmake-3.3.0-rc4.tar.gz"
-ls
-tar xfj "cmake-3.3.0-rc4.tar.gz"
+file "cmake-3.3.0-rc4.tar.gz"
+tar xfz "cmake-3.3.0-rc4.tar.gz"
 cd "cmake-3.3.0-rc4"
 mkdir build
 cd build
